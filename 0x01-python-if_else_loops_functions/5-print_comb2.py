c03dbf00e10a12c3f2c1043db1ffda8478bd954c
salma alfadili <salmafadili003@gmail.com>
2023-06-06 14:54:45 -0700

    task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    print("{:02d}".format(number), end='')
     if number == 99:
-       print("")
+        separator = "\n"
     else:
-       print(", ", end='')
+        separator = ", "
+    print("{:02d}".format(number), end=separator)
+
