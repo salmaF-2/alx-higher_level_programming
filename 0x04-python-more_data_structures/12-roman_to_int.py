833e7982782beabb3442741683468d094f994e1c
salma alfadili <salmafadili003@gmail.com>
2023-06-14 17:42:55 -0700

    task12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -15,7 +15,8 @@ def roman_to_int(roman_string):
 
     result = 0
     for i in range(len(roman_string)):
-        if i < len(roman_string) - 1 and values[roman_string[i]] < values[roman_string[i + 1]]:
+        if i < len(roman_string) - 1 and \
+           values[roman_string[i]] < values[roman_string[i + 1]]:
             result -= values[roman_string[i]]
         else:
             result += values[roman_string[i]]
