1b7cef452c0ec150828a9d6851352297a53e848f
salma alfadili <salmafadili003@gmail.com>
2023-06-26 13:53:50 -0700

    task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,10 +5,10 @@ def safe_print_list(my_list=[], x=0):
     try:
         for y in my_list:
             if nb_print < x:
-                print(y, end ="")
+                print(y, end="")
                 nb_print += 1
     except error as e:
         print("An error occurred:", e)
-    
+
     print()
     return nb_print
