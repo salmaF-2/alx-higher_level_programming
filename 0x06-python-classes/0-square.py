c5acdb2d48900d06f1109d8dd0bba034f6574132
salma alfadili <salmafadili003@gmail.com>
2023-06-27 17:09:56 -0700

    task0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+"""
+This module contains the Square class
+"""
+
+
 class Square:
+    """
+    Represents a square
+    """
     pass
+
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
