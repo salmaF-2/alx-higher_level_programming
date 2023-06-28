9bc2864e3ee2ef7d7798ddc4339601d9e079f30d
salma alfadili <salmafadili003@gmail.com>
2023-06-27 17:15:59 -0700

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains the Square class
+"""
+
+
+class Square:
+    """
+    Represents a square
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a square instance
+        Args:
+            size (int): The size of square
+        """
+        self.__size = size
