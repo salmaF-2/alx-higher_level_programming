1e21ef2e191a251616b169f26436826f9b48cbb6
salma alfadili <salmafadili003@gmail.com>
2023-07-10 15:27:28 -0700

    task1 fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-class MyList(list):
-    """MyList class"""
+"""
+prints listsorted
+"""
 
+
+class MyList(list):
+    """
+    MyList class
+    """
+    
     def print_sorted(self):
         """
         prints the list, but sorted (ascending sort)
         the elements of the list will be of type int
         """
-        sorte = sorted(self)
-        print(sorte)
+        print(sorted(self))
